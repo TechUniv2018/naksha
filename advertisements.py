27c907f8a6c2d37a11f08c9c4faacfe1e8510789
Souradeep Nanda <souradeep.nanda123@gmail.com>
2018-03-18 20:44:11 +0530

    Added real names of advertisements

--- advertisements.py
+++ advertisements.py
@@ -1,14 +1,23 @@
 import random
+import csv
+
+names = []
+
+with open('./streets.csv') as f:
+  reader = csv.reader(f)
+  for row in reader:
+    names.append(row[0])
 
 def addAds(json):
   SCALE = 1e-2
   for circle in json['heatmaps']:
     circle['advertisements'] = []
     for i, establishment in enumerate(circle['details']):
+      index = random.randint(0, len(names)-1)
       location = establishment['geometry']['location']
       circle['advertisements'].append({
-        'name': 'Placeholder Apartment %s'%(i),
-        'description': '3 BHK, price: 50,00,000 INR, Placeholder description.',
+        'name': '%s APARTMENT'%(names[index]),
+        'description': '3 BHK, price: 50,00,000 INR.',
         'lat': float(location['lat']) + (random.random() - .5) * SCALE,
         'lng': float(location['lng']) + (random.random() - .5) * SCALE
       })
